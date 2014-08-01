cc128c6c2070eae5a2b4ef57953edac20ed76a07
samgreen <greensamuelm@gmail.com>
2014-07-31 23:51:51 -0700

    Initial Commit

---
+++ Suction/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Suction
+//
+
+//  Copyright (c) 2014 Sam Green. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Suction/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Suction
+//
+//  Created by Sam Green on 7/31/14.
+//  Copyright (c) 2014 Sam Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
